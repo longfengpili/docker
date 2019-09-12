579a2cba77c5ce19dea8c2932f21019ffee142bc
longfengpili <398745129@qq.com>
2019-09-12 16:38:10 +0800

    python cron

--- python_cron/code/job_run.py
+++ python_cron/code/job_run.py
@@ -2,12 +2,13 @@
 # @Author: chunyang.xu
 # @Email:  398745129@qq.com
 # @Date:   2019-08-20 16:39:09
-# @Last Modified time: 2019-08-29 16:07:34
+# @Last Modified time: 2019-08-29 16:10:13
 
 import time
+from datetime import datetime
 
 time.sleep(1)
-print(f'{time.time()}, hello world! Dolf ! \n')
+print(f'{datetime.utcnow()}, hello world! Dolf ! \n')
 
 
 
--- python_cron/readme.md
+++ python_cron/readme.md
@@ -1,4 +1,4 @@
 # volume
  ```docker
-docker run --name mycron_v1 -v e:/GoogleDrive/work_daily/github/docker/python_cron/code:/daily_work mycron_py:v1
+docker run --name mycron_v1 -v e:/GoogleDrive/work_daily/github/docker/python_cron/code:/workspace/job/ mycron_py:v1
  ```
