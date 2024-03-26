35cc11523fe9386fe2523b70858173dd7c1e54c1
longfengpili <398745129@qq.com>
2024-03-26 10:31:06 +0800

    [删除]删除一些陈旧的内容

--- crontab/bake_cron.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# 每天对crontab 进行备份 ，同时删除最近15天的数据
-DATE=$(date +%Y%m%d)
-
-crontab -l >>/home/longfengpili/bake_cron/crontab_$DATE.bak
-find /home/longfengpili/bake_cron/ -name *.bak -mtime +7 -exec rm {} \;
