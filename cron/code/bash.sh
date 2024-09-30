99d71ebbe52b23f675f4d2acb386d161d5fea0b4
longfengpili <398745129@qq.com>
2024-09-30 11:57:20 +0800

    [优化]code

--- cron/code/bash.sh
+++ cron/code/bash.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # @Author: chunyang.xu
 # @Date:   2021-06-30 19:48:12
-# @Last Modified by:   chunyang.xu
-# @Last Modified time: 2021-06-30 19:59:29
+# @Last Modified by:   longfengpili
+# @Last Modified time: 2024-09-30 11:56:58
 
 source /etc/profile
-echo $USER
-/usr/bin/python3.8 /workspace/test.py
+echo $USERNAME
+/usr/bin/python3 /workspace/code/test.py
--- cron/code/test.py
+++ cron/code/test.py
@@ -1,12 +1,11 @@
 # -*- coding: utf-8 -*-
 # @Author: chunyang.xu
 # @Date:   2021-06-30 18:53:15
-# @Last Modified by:   chunyang.xu
-# @Last Modified time: 2021-06-30 19:04:54
+# @Last Modified by:   longfengpili
+# @Last Modified time: 2024-09-30 11:55:55
 
 
 import os
 import time
 print(f'{time.ctime()}, from test.py')
 print(os.environ)
-print(os.environ['USER'])
