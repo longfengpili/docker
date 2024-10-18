9f3738db6c4ae61632a00fc0e20aee987aa8d50c
longfengpili <398745129@qq.com>
2024-10-18 17:37:28 +0800

    [优化]目录结构

--- webhook/docker/webhook_init/startup.sh
+++ webhook/docker/webhook_init/startup.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
 # 如果本地目录为空，则从容器复制文件到本地目录
-if [ -z "$(ls -A /webhook/config)" ]; then
-   cp -r /webhook_init/config/* /webhook/config/
+if [ -z "$(ls -A /webhook)" ]; then
+   cp -r /webhook_init/* /webhook/
 fi
 
-if [ -z "$(ls -A /webhook/scripts)" ]; then
-   cp -r /webhook_init/scripts/* /webhook/scripts/
-fi
 
 # 执行 webhook 应用
 exec /usr/local/bin/webhook "$@"
